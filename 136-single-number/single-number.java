64552373ea2db9fd95e2eafd44b1ae710f33e6ab
Harinimalarkodi <harinikarthikeyan26@gmail.com>
2026-07-31 13:27:47 +0530

    Time: 1 ms (99.96%) | Memory: 46.9 MB (65.63%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            
+            k^=nums[i];
+
+            
+            
+        }
+        return k;
+
+        
+    }
+}
