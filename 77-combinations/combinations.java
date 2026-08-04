74f1271b23232df289977a6cc09465b3c91e8d4a
Harinimalarkodi <harinikarthikeyan26@gmail.com>
2026-08-04 11:31:47 +0530

    Time: 56 ms (5.06%) | Memory: 99.2 MB (5.27%) - LeetSync

---
+++ 77-combinations/combinations.java
@@ -0,0 +1,23 @@
+class Solution {
+    List<List<Integer>>a=new ArrayList<>();
+    public List<List<Integer>> combine(int n, int k) {
+        int arr[]=new int[n];
+        int index=0;
+        for(int i=1;i<=n;i++){
+            arr[index]=i;
+            index++;
+        }
+        subset(arr,0,new ArrayList<>(),k);
+        return a;
+    }
+    public void subset(int []arr,int ind,List<Integer>b,int k){
+        if(new ArrayList<>(b).size()==k){
+            a.add(new ArrayList<>(b));
+        }
+        for(int i=ind;i<arr.length;i++){
+            b.add(arr[i]);
+            subset(arr,i+1,b,k);
+            b.remove(b.size()-1);
+        }
+    }
+}
