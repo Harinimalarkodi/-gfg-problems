2bd8be22a3634cbd2d07ad516de92971c8a5620c
Harinimalarkodi <harinikarthikeyan26@gmail.com>
2026-08-04 13:49:11 +0530

    Time: 720 ms (5.12%) | Memory: 47 MB (19.40%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    List<List<Integer>>a=new ArrayList<>();
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        boolean used[]=new boolean[nums.length];
+        permute(nums,used,new ArrayList<>());
+        return a;
+        
+    }
+ public void permute(int[]nums,boolean[]used,List<Integer>b){
+    if(b.size()==nums.length&&!a.contains(new ArrayList<>(b))){
+        a.add(new ArrayList<>(b));
+    }
+    for(int i=0;i<nums.length;i++){
+        if(used[i]){
+            continue;
+        }
+        b.add(nums[i]);
+        used[i]=true;
+        permute(nums,used,b);
+        b.remove(b.size()-1);
+        used[i]=false;
+    }
+ }
+}
